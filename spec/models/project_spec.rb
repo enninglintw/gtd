4d3724e87629629c30af23def1a1ea0ed3fdce92
playpool513 <playpool513@gmail.com>
2017-07-01 20:01:13 +1000

    Add rspec for project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(
+    title: "Anything",
+    state: (0..3).to_a.sample,
+    actions_behaviour: (0..1).to_a.sample,
+  ) }
+  it { should be_valid }
+
+  describe '#title' do
+    it { should validate_presence_of(:title) }
+  end
+
+  describe '#state' do
+    it { should validate_presence_of(:state) }
+    it { should define_enum_for(:state).with([
+      :active, :later, :scheduled, :someday
+    ]) }
+  end
+
+  describe '#actions_behaviour' do
+    it { should validate_presence_of(:actions_behaviour) }
+    it { should define_enum_for(:actions_behaviour).with([:parallel, :sequential]) }
+  end
 end
