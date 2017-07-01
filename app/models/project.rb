a3bad8c3c8be50ee2482c9b0d429b2cd911cdeaa
playpool513 <playpool513@gmail.com>
2017-07-01 20:07:58 +1000

    Set validation & enum for project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ActiveRecord::Base
+  validates_presence_of :title, :state, :actions_behaviour
+
+  enum state: [:active, :later, :scheduled, :someday]
+  enum actions_behaviour: [:parallel, :sequential]
 end
