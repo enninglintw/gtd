b18571135fe595898374627bd1fc9293b1419428
playpool513 <playpool513@gmail.com>
2017-07-01 22:32:03 +1000

    $ rails g model project_tag project_id:integer:index tag_id:integer:index; rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701121210) do
+ActiveRecord::Schema.define(version: 20170701123115) do
+
+  create_table "project_tags", force: :cascade do |t|
+    t.integer  "project_id", limit: 4
+    t.integer  "tag_id",     limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "project_tags", ["project_id"], name: "index_project_tags_on_project_id", using: :btree
+  add_index "project_tags", ["tag_id"], name: "index_project_tags_on_tag_id", using: :btree
 
   create_table "projects", force: :cascade do |t|
     t.string   "title",             limit: 255
