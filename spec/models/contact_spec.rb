5a7277dd8b93b18197486ab7ea0dffc09aa3e22e
playpool513 <playpool513@gmail.com>
2017-07-03 18:30:03 +1000

    Add rspec for tag & subclasses

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 require 'support/tag_sti_helper'
 
-RSpec.describe Tag, type: :model do
+RSpec.describe Contact, type: :model do
   it_should_behave_like "an STI class of tag"
 end
