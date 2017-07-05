8b11d72ac0b844cb6533e29c1f9be35e5c3f3e2e
playpool513 <playpool513@gmail.com>
2017-07-05 21:32:13 +1000

    Initialize & set state of todos when Body is loaded, and pass it down to AllTodos

--- app/assets/javascripts/components/_all_todos.js.jsx
+++ app/assets/javascripts/components/_all_todos.js.jsx
@@ -1,17 +1,6 @@
 var AllTodos = React.createClass({
-  getInitialState() {
-    return { todos: [] }
-  },
-
-  componentDidMount() {
-    $.getJSON(
-      '/api/v1/todos.json',
-      (response) => { this.setState({ todos: response }) }
-    );
-  },
-
   render() {
-    var todos = this.state.todos.map((todo) => {
+    var todos = this.props.todos.map((todo) => {
       return (
         <div key={todo.id}>
           <h3>{todo.title}</h3>
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,9 +1,20 @@
 var Body = React.createClass({
+  getInitialState() {
+    return { todos: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON(
+      '/api/v1/todos.json',
+      (response) => { this.setState({ todos: response }) }
+    );
+  },
+
   render() {
     return (
       <div>
         <NewTodo />
-        <AllTodos />
+        <AllTodos todos={this.state.todos} />
       </div>
     )
   }
