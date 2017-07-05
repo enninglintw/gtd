81800a134bf2fa99f5d89d4bb67a49051b6dec7b
playpool513 <playpool513@gmail.com>
2017-07-05 20:42:37 +1000

    Fetch values in input columns when clicking submit button

--- app/assets/javascripts/components/_new_todo.js.jsx
+++ app/assets/javascripts/components/_new_todo.js.jsx
@@ -1,10 +1,18 @@
 var NewTodo = React.createClass({
+  handleCreate() {
+    var title = this.refs.title.value;
+    var notes = this.refs.notes.value;
+
+    console.log('The title value is ' + title);
+    console.log('The notes value is ' + notes);
+  },
+
   render() {
     return (
       <div>
-        <input placeholder='New todo' />
-        <input placeholder='Enter notes' />
-        <button>Submit</button>
+        <input ref='title' placeholder='New todo' />
+        <input ref='notes' placeholder='Enter notes' />
+        <button onClick={this.handleCreate}>Submit</button>
       </div>
     )
   }
