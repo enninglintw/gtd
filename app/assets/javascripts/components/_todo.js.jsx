667a9bee42957b42ab1e41cf5b20ef42f9c54ebe
playpool513 <playpool513@gmail.com>
2017-07-06 19:55:50 +1000

    Fetch values in input columns when editing

--- app/assets/javascripts/components/_todo.js.jsx
+++ app/assets/javascripts/components/_todo.js.jsx
@@ -4,13 +4,21 @@ var Todo = React.createClass({
   },
 
   handleEdit() {
+    if(this.state.editable) {
+      var title = this.refs.title.value;
+      var notes = this.refs.notes.value;
+      console.log('In edit mode');
+      console.log(this.state.editable);
+      console.log(title);
+      console.log(notes);
+    }
     this.setState({ editable: !this.state.editable });
   },
 
   render() {
-    var title = this.state.editable ? <input type='text' defaultValue={this.props.todo.title}/> : <h3>{this.props.todo.title}</h3>;
+    var title = this.state.editable ? <input type='text' ref='title' defaultValue={this.props.todo.title}/> : <h3>{this.props.todo.title}</h3>;
 
-    var notes = this.state.editable ? <input type='text' defaultValue={this.props.todo.notes}/> : <p>{this.props.todo.notes}</p>;
+    var notes = this.state.editable ? <input type='text' ref='notes' defaultValue={this.props.todo.notes}/> : <p>{this.props.todo.notes}</p>;
 
     var editButton = this.state.editable ? <button onClick={this.handleEdit}>Save Changes</button> : <button onClick={this.handleEdit}>Edit</button>;
 
