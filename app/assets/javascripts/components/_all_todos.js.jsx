cde93816a819c6199b6b7c57e43c10f5a21bf630
playpool513 <playpool513@gmail.com>
2017-07-06 18:16:01 +1000

    Print message in console to show edit button is clicked

--- app/assets/javascripts/components/_all_todos.js.jsx
+++ app/assets/javascripts/components/_all_todos.js.jsx
@@ -9,13 +9,17 @@ var AllTodos = React.createClass({
     });
   },
 
+  handleEdit() {
+    console.log('Edit button is clicked.');
+  },
+
   render() {
     var todos = this.props.todos.map((todo) => {
       return (
         <div key={todo.id}>
           <h3>{todo.title}</h3>
           <p>{todo.notes}</p>
-          <button>Edit</button>
+          <button onClick={this.handleEdit}>Edit</button>
           <button onClick={this.handleDelete.bind(this, todo.id)}>Delete</button>
         </div>
       )
