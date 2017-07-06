f885989ff2ef3a887b0b029f48b46b879c4aef68
playpool513 <playpool513@gmail.com>
2017-07-06 15:33:10 +1000

    Print message in console to show delete button is clicked

--- app/assets/javascripts/components/_all_todos.js.jsx
+++ app/assets/javascripts/components/_all_todos.js.jsx
@@ -1,11 +1,15 @@
 var AllTodos = React.createClass({
+  handleDelete() {
+    console.log('Delete button is clicked.');
+  },
+
   render() {
     var todos = this.props.todos.map((todo) => {
       return (
         <div key={todo.id}>
           <h3>{todo.title}</h3>
           <p>{todo.notes}</p>
-          <button>Delete</button>
+          <button onClick={this.handleDelete}>Delete</button>
         </div>
       )
     });
