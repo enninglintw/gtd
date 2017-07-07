334f3012f67a94ccc08ee0635822473e06432a59
playpool513 <playpool513@gmail.com>
2017-07-07 21:27:22 +1000

    Fetch values in input columns when clicking submit button

--- app/assets/javascripts/components/_new_project.js.jsx
+++ app/assets/javascripts/components/_new_project.js.jsx
@@ -1,11 +1,19 @@
 var NewProject = React.createClass({
+  handleCreate() {
+    var title = this.refs.title.value;
+    var notes = this.refs.notes.value;
+
+    console.log(`Title: ${title}`);
+    console.log(`Notes: ${notes}`);
+  },
+
   render() {
     return (
       <div>
         <h2>Create new project</h2>
-        <input placeholder='New project'></input>
-        <input placeholder='Enter notes'></input>
-        <button>Submit</button>
+        <input ref='title' placeholder='New project'></input>
+        <input ref='notes' placeholder='Enter notes'></input>
+        <button onClick={this.handleCreate}>Submit</button>
       </div>
     )
   }
