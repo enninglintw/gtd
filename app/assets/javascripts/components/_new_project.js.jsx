64cd99ecff44e74cfc715fa0eac3cf93319157cc
playpool513 <playpool513@gmail.com>
2017-07-07 21:19:04 +1000

    Add input columns & submit button in NewProject

--- app/assets/javascripts/components/_new_project.js.jsx
+++ app/assets/javascripts/components/_new_project.js.jsx
@@ -3,6 +3,9 @@ var NewProject = React.createClass({
     return (
       <div>
         <h2>Create new project</h2>
+        <input placeholder='New project'></input>
+        <input placeholder='Enter notes'></input>
+        <button>Submit</button>
       </div>
     )
   }
