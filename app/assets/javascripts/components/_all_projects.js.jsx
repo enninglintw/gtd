e2dd512950ae495f2c13835f6559aa45d47b047d
playpool513 <playpool513@gmail.com>
2017-07-07 20:53:34 +1000

    Render projects in AllProjects

--- app/assets/javascripts/components/_all_projects.js.jsx
+++ app/assets/javascripts/components/_all_projects.js.jsx
@@ -11,9 +11,23 @@ var AllProjects = React.createClass({
   },
 
   render() {
+    var projects = this.state.projects.map((project) => {
+      return (
+        <div key={project.id}>
+          <div>{project.focus ? '★' : '☆'}</div>
+          <div>{project.state}</div>
+          <div>{project.actions_behaviour}</div>
+          <div>{project.due}</div>
+          <h3>{project.title}</h3>
+          <p>{project.notes}</p>
+        </div>
+      )
+    });
+
     return (
       <div>
         <h2>Projects</h2>
+        {projects}
       </div>
     )
   }
