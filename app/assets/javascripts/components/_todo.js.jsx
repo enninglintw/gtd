1d211cacfed6d17453326f15746cbf5464dab297
playpool513 <playpool513@gmail.com>
2017-07-09 20:44:35 +1000

    Fix spacing

--- app/assets/javascripts/components/_todo.js.jsx
+++ app/assets/javascripts/components/_todo.js.jsx
@@ -5,10 +5,10 @@ var Todo = React.createClass({
 
   handleEdit() {
     if(this.state.editable) {
-      var id = this.props.todo.id;
+      var id    = this.props.todo.id;
       var title = this.refs.title.value;
       var notes = this.refs.notes.value;
-      var todo = { id: id, title: title, notes: notes };
+      var todo  = { id: id, title: title, notes: notes };
 
       $.ajax({
         url: `/api/v1/todos/${id}`,
@@ -25,9 +25,7 @@ var Todo = React.createClass({
 
   render() {
     var title = this.state.editable ? <input type='text' ref='title' defaultValue={this.props.todo.title}/> : <h3>{this.props.todo.title}</h3>;
-
     var notes = this.state.editable ? <input type='text' ref='notes' defaultValue={this.props.todo.notes}/> : <p>{this.props.todo.notes}</p>;
-
     var editButton = this.state.editable ? <button onClick={this.handleEdit}>Save Changes</button> : <button onClick={this.handleEdit}>Edit</button>;
 
     return (
