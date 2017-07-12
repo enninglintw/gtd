d122b867b3b8d3103e08045618e41c21c3d78236
playpool513 <playpool513@gmail.com>
2017-07-12 19:13:22 +1000

    Print message after Inbox is clicked

--- app/assets/javascripts/components/_todo_types.js.jsx
+++ app/assets/javascripts/components/_todo_types.js.jsx
@@ -1,9 +1,13 @@
 var TodoTypes = React.createClass({
+  renderInbox() {
+    console.log('Inbox is clicked');
+  },
+
   render() {
     return (
       <div>
         <h2>COLLECT</h2>
-        <h3>Inbox</h3>
+        <h3 onClick={this.renderInbox}>Inbox</h3>
         <h2>ACTIONS</h2>
         <h3>Next</h3>
         <h3>Later</h3>
