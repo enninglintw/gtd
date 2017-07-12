b8590fc27290832dfdf8a19efdeeece7496aeb3c
playpool513 <playpool513@gmail.com>
2017-07-12 19:16:49 +1000

    Move renderInbox from TodoTypes to Main

--- app/assets/javascripts/components/_todo_types.js.jsx
+++ app/assets/javascripts/components/_todo_types.js.jsx
@@ -1,13 +1,9 @@
 var TodoTypes = React.createClass({
-  renderInbox() {
-    console.log('Inbox is clicked');
-  },
-
   render() {
     return (
       <div>
         <h2>COLLECT</h2>
-        <h3 onClick={this.renderInbox}>Inbox</h3>
+        <h3 onClick={this.props.renderInbox}>Inbox</h3>
         <h2>ACTIONS</h2>
         <h3>Next</h3>
         <h3>Later</h3>
