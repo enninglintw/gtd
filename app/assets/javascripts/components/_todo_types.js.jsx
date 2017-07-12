f20eff177c9a87a5f0a0433b3c2fc280844106a6
playpool513 <playpool513@gmail.com>
2017-07-12 19:10:12 +1000

    Render TodoTypes in Sidebar

---
+++ app/assets/javascripts/components/_todo_types.js.jsx
@@ -0,0 +1,16 @@
+var TodoTypes = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>COLLECT</h2>
+        <h3>Inbox</h3>
+        <h2>ACTIONS</h2>
+        <h3>Next</h3>
+        <h3>Later</h3>
+        <h3>Waiting</h3>
+        <h3>Scheduled</h3>
+        <h3>Someday</h3>
+      </div>
+    )
+  }
+});
